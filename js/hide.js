3439fd3360b01e483c90bc5cfa0ee6a1a483978f
Dan Crews <dancrews@me.com>
2014-01-15 19:16:00 -0500

    simple JavaScript event listener

---
+++ js/hide.js
@@ -0,0 +1,20 @@
+/* save the two nodes into variables */
+var button = document.getElementById("hide"),
+    target = document.getElementById("target");
+
+/* define what we want to do in a function */
+function hide() {
+  target.setAttribute("class","hide");
+}
+
+/* add the css class when the button is clicked */
+button.addEventListener("click", hide, false);
+
+/* You know JavaScript is enabled at this point because none of this would apply otherwise */
+
+/* check if localStorage is supported */
+if(typeof window.localStorage !== "undefined") {
+  // use localStorage
+} else {
+  // use normal cookies
+}
