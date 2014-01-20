c2328fd4458016e9234e13df1d13d2206e1714d2
Dan Crews <dancrews@me.com>
2014-01-20 17:17:28 -0500

    added getElementsByClassName

--- access_dom/js/dom_examples.js
+++ access_dom/js/dom_examples.js
@@ -15,3 +15,25 @@ document.getElementsByTagName("p").length;
 if(document.getElementsByTagName("p").length > 0) {
   // it returns "3", and 3 is greater than "0", so we're good to go.
 }
+
+/* Using getElementsByTagName to target a single node */
+// the first paragraph using the item method
+document.getElementsByTagName("p").item(0);
+
+// the first paragraph with array syntax
+document.getElementsByTagName("p")[0];
+
+// the second paragraph with the item method
+document.getElementsByTagName("p").item(1);
+
+// the second paragraph with array syntax
+document.getElementsByTagName("p")[1];
+
+// returns all elements with a class of dropcap
+document.getElementsByClassName("dropcap");
+
+// returns all elements with BOTH "dropcap" and "huge" classes
+document.getElementsByClassName("dropcap huge");
+
+// returns all elements classed with "huge" that are inside elements ID'd with "e"
+document.getElementById("e").getElementsByClassName("huge");
