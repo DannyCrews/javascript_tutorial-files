7e7d9062a8ec3eab4a55bd4265abe5feb1f6bda5
Dan Crews <dancrews@me.com>
2014-01-20 17:32:36 -0500

    added querySelector and QuerySelectorAll

--- access_dom/js/dom_examples.js
+++ access_dom/js/dom_examples.js
@@ -37,3 +37,23 @@ document.getElementsByClassName("dropcap huge");
 
 // returns all elements classed with "huge" that are inside elements ID'd with "e"
 document.getElementById("e").getElementsByClassName("huge");
+
+/* Using querySelectors */
+// get the header ID element
+document.querySelector("#header");
+
+// get the first element with a dropcap class
+document.querySelector(".dropcap");
+
+// get all the elements with a dropcap class - returns a NodeList
+document.querySelectorAll(".dropcap");
+
+// get all elements with a class of "dropcap" OR "huge"
+document.querySelectorAll(".dropcap, .huge");
+
+// get all paragraphs that have a class
+document.querySelectorAll("p[class]");
+
+
+
+
