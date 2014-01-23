54e55f267adc0b7ccd0bc33fb08dadbf60b29629
Dan Crews <dancrews@me.com>
2014-01-23 16:57:12 -0500

    adding examples to function.js

--- functions.js
+++ functions.js
@@ -6,3 +6,79 @@ function sayHello() {
 /* calling a function */
 sayHello(); // hey there
 
+/* passing a function variable through arguements */
+
+/* declare the function */
+function sayHello(message){
+  alert(message); // message is also an arguement in the 'alert' method
+}
+
+/* call it a couple times with different messages */
+sayHello("Hey there, you stink!");
+sayHello("I feel bad I just said that.");
+
+
+/* Define an anonymous function */
+(function () {
+  /* define a variable inside the function */
+  var greeting = "Hello, Dan";
+  /* access the variable inside the function */
+  alert("in scope: " + greeting);
+})(); // end anonymous function
+
+/* try and access the variable outside the function scope */
+alert("out of scope: " + typeof(greeting)); // alerts "undefined"
+
+
+/* using a callback function */
+function sayHello(message) {
+  alert(message);
+}
+
+(function (){
+  var greeting = "Welcome",
+      exitStatement = "ok, please leave.";
+
+      sayHello(greeting);
+      sayHello(exitStatement);
+})();
+
+
+/* returning data with a function */
+function sayHello(message) {
+  return message + "!"; //add some emotion, too
+}
+
+
+/* returning multiple data values with a function */
+function sayHello(greeting, exitStatement){
+  /* add some passion */
+  var newGreeting = greeting + "!",
+      newExitStatement = exitStatement + "!!";
+/* return the arguments in an array */
+return [newGreeting, newExitStatement];
+}
+
+/* using returned function values passed into another function */
+function startle(polite, rude){
+  /* call the sayHello function */
+  var greeting = sayHello(polite, rude)[0],
+      exit = sayHello(polite, rude)[1];
+  /* alert the variables that have been passed through each function */
+  alert(greeting + " -- " + exit);
+}
+
+/* call the function with our arguements defined */
+startle("thank you", "you stink");
+
+
+
+
+
+
+
+
+
+
+
+
