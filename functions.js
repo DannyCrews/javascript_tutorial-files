eb793832ce142d81ea9a68b0e0220f12b58d1699
Dan Crews <dancrews@me.com>
2014-03-03 21:05:33 -0500

    More javascript

--- functions.js
+++ functions.js
@@ -1,96 +1,19 @@
-/* Writing a basic function */
-function sayHello() {
-  alert("hey there!");
+//Function Definitions
+
+function displayGreeting()
+{
+    document.write("Hey there, dude!");
 }
 
-/* calling a function */
-sayHello(); // hey there
-
-/* passing a function variable through arguements */
-
-/* declare the function */
-function sayHello(message){
-  alert(message); // message is also an arguement in the 'alert' method
+function displayPlayerScore(theScore)
+{
+    //playerScore = 1000;
+    theScore += 1000;  //theScore = theScore + 1000;
+    document.write("<br/>Player score: " + theScore);
 }
 
-/* call it a couple times with different messages */
-sayHello("Hey there, you stink!");
-sayHello("I feel bad I just said that.");
-
-
-/* Define an anonymous function */
-(function () {
-  /* define a variable inside the function */
-  var greeting = "Hello, Dan";
-  /* access the variable inside the function */
-  alert("in scope: " + greeting);
-})(); // end anonymous function
-
-/* try and access the variable outside the function scope */
-alert("out of scope: " + typeof(greeting)); // alerts "undefined"
-
-
-/* using a callback function */
-function sayHello(message) {
-  alert(message);
+function addTheseNumbers(x,y)
+{
+    //document.write(x+y);
+    return (x+y);
 }
-
-(function (){
-  var greeting = "Welcome",
-      exitStatement = "ok, please leave.";
-
-      sayHello(greeting);
-      sayHello(exitStatement);
-})();
-
-
-/* returning data with a function */
-function sayHello(message) {
-  return message + "!"; //add some emotion, too
-}
-
-
-/* returning multiple data values with a function */
-function sayHello(greeting, exitStatement){
-  /* add some passion */
-  var newGreeting = greeting + "!",
-      newExitStatement = exitStatement + "!!";
-/* return the arguments in an array */
-return [newGreeting, newExitStatement];
-}
-
-/* using returned function values passed into another function */
-function startle(polite, rude){
-  /* call the sayHello function */
-  var greeting = sayHello(polite, rude)[0],
-      exit = sayHello(polite, rude)[1];
-  /* alert the variables that have been passed through each function */
-  alert(greeting + " -- " + exit);
-}
-
-/* call the function with our arguements defined */
-startle("thank you", "you stink");
-
-
-/* grouping similar functions - functions stored in an object are called methods\ */
-var addressBookMethods = {
-  sayHello: function(message){
-    return message;
-  },
-  startle: function(){
-    alert(addressBookMethods.sayHello("hey there, called from a method"));
-  }
-}
-/* call the function */
-addressBookMethods.startle();
-
-
-
-
-
-
-
-
-
-
-
